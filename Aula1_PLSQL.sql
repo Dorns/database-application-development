1ecbd87a432e042492dd4db4a1096289a11e448a
logonrm <logonrm@fiap.com.br>
2017-03-14 20:33:38 -0300

    Estuturas de Controle

--- Aula1_PLSQL.sql
+++ Aula1_PLSQL.sql
@@ -1,7 +1,7 @@
 set serveroutput on; --Habilita retorno da console do server
 
 DECLARE 
-  mensagem VARCHAR2(100) := 'Hello World!';
+  v_mensagem VARCHAR2(100) := 'Hello World!';
 BEGIN
   DBMS_OUTPUT.put_line(mensagem);
 END;
@@ -10,22 +10,22 @@ END;
 --Soma, Multiplicação e Divisão de 2 numeros.
 
 DECLARE
-  v1 NUMBER := 5;
-  v2 NUMBER := 7;
-  soma NUMBER;
-  multiplicacao NUMBER;
-  divisao NUMBER;
+  v1 NUMBER := 10;
+  v2 NUMBER := 3;
+  v_soma NUMBER;
+  v_multiplicacao NUMBER;
+  v_divisao NUMBER;
 BEGIN
-  soma := (v1+v2);
-  multiplicacao := (v1*v2);
-  divisao := (v1/v2);
+  v_soma := (v1+v2);
+  v_multiplicacao := (v1*v2);
+  v_divisao := (v1/v2);
   DBMS_OUTPUT.put_line('Entrada v1:' || v1 || ' e v2:' || v2 || ' Soma: ' || 
   soma || '; Multiplicação: ' || multiplicacao || '; Divisão: ' || divisao);
 END;
  
 --Mostrar apenas o primeiro nome com uma variavel de nome completo 
 DECLARE 
-  nome_completo VARCHAR2(100) := 'Felipe Dornelas Viel';
+  v_nome_completo VARCHAR2(100) := 'Felipe Dornelas Viel';
 BEGIN
   DBMS_OUTPUT.put_line(SUBSTR(nome_completo, 0, INSTR(nome_completo, ' ') -1));
 END;
@@ -33,7 +33,8 @@ END;
 --receba uma data no formato DD/MM/YYYY e mostre só o mes por extenso
 
 DECLARE 
-  data DATE := '10/01/1995';
+  v_data DATE;
 BEGIN
-  DBMS_OUTPUT.put_line(data, 'MM');
+  v_data := TO_DATE('10/01/1995', 'dd/mm/yyyy');
+  DBMS_OUTPUT.put_line(TO_CHAR(v_data, 'MONTH'));
 END;
